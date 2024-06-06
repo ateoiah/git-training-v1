05d5ef6a93fa4d63958ab3c1c3cc047b81ead152
bryan <kiluashkin@gmail.com>
2024-06-05 22:28:52 -0800

    2nd commit

---
+++ ex1-forloop.py
@@ -0,0 +1,8 @@
+# Define a list of numbers
+numbers = [1, 2, 3, 4, 5]
+
+# Use a for loop to iterate through the list
+for number in numbers:
+    print(number)
+
+print("first modification in 2nd commit")
---
+++ ex2-dfunction.py
@@ -0,0 +1,12 @@
+def sum_of_numbers(numbers):
+    total = 0
+    for number in numbers:
+        total += number
+    return total
+
+# Example usage
+numbers = [1, 2, 3, 4, 5]
+result = sum_of_numbers(numbers)
+print("The sum of the numbers is:", result)
+
+print("first modification in 2nd commit")
